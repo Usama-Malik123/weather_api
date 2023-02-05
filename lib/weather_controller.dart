d0ea90fe25d6ae4e6c610cf2b5c98e64bb4ed036
Usama-Malik123 <98714119+Usama-Malik123@users.noreply.github.com>
2023-02-06 00:25:57 +0500

    first commit

---
+++ lib/weather_controller.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+import 'package:weather_app/snack_bar.dart';
+import 'package:weather_app/weather_model.dart';
+import 'package:weather_app/weather_service.dart';
+
+class WeatherController extends GetxController {
+  final weatherService = Get.put(WeatherService());
+
+  Future<Weather> getWeatherData() async {
+    var res;
+    try {
+      res = await weatherService.getWeather();
+      if (res.statusCode != 200 || res.statusCode != 201) {
+      
+        return WeatherSnackBars.errorSnackBar(message: res.data['message']);
+      } else {}
+    } catch (e) {
+      WeatherSnackBars.errorSnackBar(message: e.toString());
+    }
+    return Weather.fromJson(res.data);
+  }}
